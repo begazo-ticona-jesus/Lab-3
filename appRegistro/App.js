2f8ae7b18c8f7599e55071106e8a4e05317adf52
begazo-ticona-jesus <jbegazoti@unsa.edu.pe>
2023-09-30 18:51:15 -0500

    pequeños cambios

--- appRegistro/App.js
+++ appRegistro/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import CursoList from './screens/homeScreen';
-import MatriculaForm from './screens/matriculaFormScreen';
-import MatriculaList from './screens/matriculaListScreen';
+import Navigation from './components/commonNavigator';
 
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={CursoList} />
-        <Tab.Screen name="MatriculaForm" component={MatriculaForm} />
-        <Tab.Screen name="MatriculaList" component={MatriculaList} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Navigation />
   );
 }
---
+++ appRegistro/components/commonNavigator.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import CursoList from '../screens/homeScreen';
+import MatriculaForm from '../screens/matriculaFormScreen';
+import MatriculaList from '../screens/matriculaListScreen';
+
+
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Home" component={CursoList} />
+                <Tab.Screen name="MatriculaForm" component={MatriculaForm} />
+                <Tab.Screen name="MatriculaList" component={MatriculaList} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
